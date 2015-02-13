9a3dee13e6b33fdba24bbfcdaa111097f6a2772d
XadillaX <admin@xcoder.in>
2015-02-13 15:27:56 +0800

    allow history

--- xto.js
+++ xto.js
@@ -4,10 +4,87 @@
  * Copyright (c) 2015 Huaban.com, all rights
  * reserved
  */
+require("sugar");
 var xto = require("xto");
-var AlfredItem = require("alfred-item");
 var companyMap = require("xto/const/namemapper");
+var AlfredItem = require("alfred-item");
 var han = require("han");
+var async = require("async");
+var fs = require("fs-extra");
+var path = require("path");
+
+var HISTORY_PATH = path.join(process.env.HOME, ".alfred/xto/history");
+var SHOW_MAX_HISTORY = 8;
+
+function writeResult(company, num, err, info) {
+    if(err && (err.message.indexOf("不存在") < 0 || err.message.indexOf("过期") < 0)) {
+        return;
+    }
+
+    async.waterfall([
+        function(callback) {
+            fs.ensureFile(HISTORY_PATH, function(err) {
+                callback(err);
+            });
+        },
+
+        function(callback) {
+            fs.readJson(HISTORY_PATH, function(err, json) {
+                return callback(undefined, json || {});
+            });
+        },
+
+        function(orig, callback) {
+            var code = company.code;
+            if(undefined === orig[code]) orig[code] = [];
+            var inner = orig[code].reduce(function(res, info) {
+                if(info.num !== num) {
+                    res.push(info);
+                }
+
+                return res;
+            }, []);
+
+            var cur = { num: num };
+            var updateTime = Date.create().format("{yyyy}年{MM}月{dd}日 {HH}:{mm}:{ss}");
+            if(err) {
+                cur.title = num + "：" + err.message;
+            } else if(info !== undefined && info !== null) {
+                cur.title = num + "：" + info.data[0].context;
+            }
+
+            if(!cur.title) return callback(undefined, orig);
+
+            cur.subtitle = "最后更新：" + updateTime;
+            if(info) {
+                cur.subtitle = xto.stateToText(info.state) + "，" + cur.subtitle;
+            }
+            cur.time = updateTime;
+            cur.complete = company.shortname;
+            if(cur.complete.indexOf(" ") >= 0) {
+                cur.complete = company.code;
+            }
+            cur.complete += " " + num;
+            inner.unshift(cur);
+
+            orig[code] = inner;
+
+            while(orig[code].length > SHOW_MAX_HISTORY) orig[code].pop();
+
+            callback(undefined, orig);
+        },
+
+        function(json, callback) {
+            fs.writeJson(HISTORY_PATH, json, callback);
+        }
+    ], function(err) {
+        // 有 err 也没办法
+        if(err) {
+            /** ignore */
+            err = null;
+        }
+    });
+}
 
 function outputWait() {
     var item = new AlfredItem();
@@ -59,7 +136,7 @@ function pinyinMatched(src, dist) {
     return true;
 }
 
-function completeCompanies(name) {
+function searchCompanies(name) {
     var results = [];
     for(var i = 0; i < companyMap.length; i++) {
         var company = companyMap[i];
@@ -74,6 +151,11 @@ function completeCompanies(name) {
         }
     }
 
+    return results;
+}
+
+function completeCompanies(name) {
+    var results = searchCompanies(name);
     if(!results.length) return outputNoCompany(name);
 
     var item = new AlfredItem();
@@ -93,7 +175,31 @@ function completeCompanies(name) {
     console.log(item);
 }
 
+function outputHistory(company, item, maxItems, prefix) {
+    if(!maxItems) return console.log(item);
+
+    prefix = prefix.toLowerCase();
+    fs.readJson(HISTORY_PATH, function(err, json) {
+        if(json) {
+            var c = json[company.code];
+            for(var i = 0; i < Math.min(c.length, maxItems); i++) {
+                if(!c[i].num.toLowerCase().startsWith(prefix)) continue;
+                item.addItem(
+                    i + 1,
+                    "【" + company.companyname + "】" + c[i].title,
+                    c[i].subtitle,
+                    "icon.png", {
+                        autocomplete: c[i].complete
+                    });
+            }
+        }
+
+        return console.log(item);
+    });
+}
+
 module.exports = function(query) {
+    var rawQuery = query;
     var query = query.split(" ").compact(true);
     var item = new AlfredItem();
 
@@ -107,6 +213,10 @@ module.exports = function(query) {
     if(!company) {
         if(query.length === 1) {
             return completeCompanies(companyName);
+        } else if(query.length > 1) {
+            var companies = searchCompanies(companyName);
+            if(companies.length !== 1) return outputNoCompany(companyName);
+            company = companies[0];
         } else {
             return outputNoCompany(companyName);
         }
@@ -122,7 +232,8 @@ module.exports = function(query) {
             "icon.png", {
                 autocomplete: displayName + " "
             });
-        return console.log(item);
+
+        return outputHistory(company, item, rawQuery.endsWith(" ") ? SHOW_MAX_HISTORY : 0, "");
     }
 
     var num = query[1];
@@ -134,11 +245,14 @@ module.exports = function(query) {
             "【" + company.companyname + "】查查 " + num + " 到哪了",
             "等待输入完整运单号",
             "icon.png");
-        return console.log(item);
+
+        return outputHistory(company, item, SHOW_MAX_HISTORY - 1, num);
     }
 
     // 查询快递
     xto.query(num, company.code, function(err, info) {
+        writeResult(company, num, err, info);
+
         if(err) {
             return outputError(err);
         }
